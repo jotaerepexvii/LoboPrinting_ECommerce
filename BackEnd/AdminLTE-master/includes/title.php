77c142ca80dcd386dda2e3b299b6849cdb50f371
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-03-03 23:07:30 -0400

    Tweaks
    Se añadió lobo.ico y se colocó en lugar del logo del template.

--- BackEnd/AdminLTE-master/includes/title.php
+++ BackEnd/AdminLTE-master/includes/title.php
@@ -1,1 +1,2 @@
 <title>Lobo Printing | Dashboard</title>
+<link rel="icon"  href="dist/img/lobo.ico" type="icon" sizes="16x16">
--- BackEnd/AdminLTE-master/index3.php
+++ BackEnd/AdminLTE-master/index3.php
@@ -35,7 +35,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.php" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="dist/img/lobo.ico" alt="Lobo Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Menu</span>
     </a>
 
