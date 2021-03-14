ba1f8e38d611a8d6ef398c3b4900448afe00cb56
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-03-13 21:20:30 -0400

    Front End

---
+++ FrontEnd/LoboPrinting/php/footer.php
@@ -0,0 +1,77 @@
+<!-- FOOTER AREA START -->
+<footer class="htc__foooter__area gray-bg">
+    <div class="container">
+        <div class="row">
+            <div class="footer__container clearfix">
+                 <!-- SINGLE FOOTER WIDGET START -->
+                <div class="col-md-3 col-lg-6 col-sm-6">
+                    <div class="ft__widget">
+                        <div class="ft__logo">
+                            <a href="index.php">
+                                <img src="images/logo/logo.png" alt="footer logo">
+                            </a>
+                        </div>
+                        
+                    </div>
+                </div>
+                <!-- SINGLE FOOTER WIDGET END -->
+                <!-- SINGLE FOOTER WIDGET START -->
+                <div class="col-md-3 col-lg-6 col-sm-6 smt-30 xmt-30">
+                    <div class="ft__widget">
+                        <h2 class="ft__title">Infomation</h2>
+                        <div class="footer-address">
+                            <ul>
+                                <li>
+                                    <div class="address-icon">
+                                        <i class="zmdi zmdi-pin"></i>
+                                    </div>
+                                <div class="address-text">
+                                        <p>194 Main Rd T, FS Rayed <br> VIC 3057, USA</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="address-icon">
+                                        <i class="zmdi zmdi-email"></i>
+                                    </div>
+                                    <div class="address-text">
+                                        <a href="#"> info@example.com</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="address-icon">
+                                        <i class="zmdi zmdi-phone-in-talk"></i>
+                                    </div>
+                                    <div class="address-text">
+                                        <p>+012 345 678 102 </p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <ul class="social__icon">
+                            <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-instagram"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- SINGLE FOOTER WIDGET END -->
+            </div>
+        </div>
+        <!-- COPYRIGHT AREA START -->
+        <div class="htc__copyright__area">
+            <div class="row">
+                <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                    <div class="copyright__inner">
+                        <div class="copyright">
+                            <p>© 2017 <a href="https://freethemescloud.com/">Free themes Cloud</a>
+                            All Right Reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- COPYRIGHT AREA END -->
+    </div>
+</footer>
+<!-- FOOTER AREA END -->
---
+++ FrontEnd/LoboPrinting/php/header.php
@@ -0,0 +1,67 @@
+<!-- HEADER STYLE START -->
+<header id="header" class="htc-header header--3 bg__white">
+     <!-- MAINMENU AREA START -->
+    <div id="sticky-header-with-topbar" class="mainmenu__area sticky__header" style="background: gold;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-lg-2 col-sm-3 col-xs-3">
+                    <div class="logo">
+                        <a href="index.php">
+                            <img src="images/logo/logo.png" alt="logo">
+                        </a>
+                    </div>
+                </div>
+                <!-- MAINMENU ARES START -->
+                <div class="col-md-8 col-lg-8 col-sm-6 col-xs-6">
+                    <!-- MENU START -->
+                    <nav class="mainmenu__nav hidden-xs hidden-sm">
+                        <ul class="main__menu">
+                            <li class="drop"><a href="index.php">Inicio</a></li>
+                            <li class="drop"><a href="services.php">Servicios</a>
+                                <ul class="dropdown">
+                                    <li><a href="copies.php">Copias</a></li>
+                                    <li><a href="photos.php">Fotos</a></li>
+                                    <li><a href="bound.php">Encuadernados</a></li>
+                                </ul>
+                            </li>
+                        <li class="drop"><a href="products.php">Productos</a></li>
+                            <li class="drop"><a href="about_us.php">Sobre Nosotros</a></li>
+                            <li class="drop"><a href="contact_us.php">Contactanos</a></li>
+                        </ul>
+                    </nav>
+                    <!-- MENU END -->
+                    <!-- MOBILE MENU START -->
+                    <div class="mobile-menu clearfix visible-xs visible-sm">
+                        <nav id="mobile_dropdown">
+                            <ul>
+                                <li><a href="index.php">Inicio</a></li>
+                                <li><a href="services.php">Servicios</a>
+                                    <ul>
+                                        <li><a href="copies.php">Copias</a></li>
+                                        <li><a href="photos.php">Fotos</a></li>
+                                        <li><a href="bound.phpl">Encuadernados</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="products.php">Productos</a></li>
+                                <li><a href="about_us.php">Sobre Nosotros</a></li>
+                                <li><a href="contact_us.php">Contactanos</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!-- MOBILE MENU END -->
+                </div>
+                <!-- MAINMENU ARES END -->
+                <div class="col-md-2 col-sm-4 col-xs-3">  
+                    <ul class="menu-extra">
+                        <li><a href="login-register.html"><span class="ti-user"></span></a></li>
+                        <li class="cart__menu"><span class="ti-shopping-cart"></span></li>
+                        <li class="toggle__menu hidden-xs hidden-sm"><span class="ti-menu"></span></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="mobile-menu-area"></div>
+        </div>
+    </div>
+     <!-- MAINMENU AREA END -->
+</header>
+<!-- HEADER STYLE END -->
