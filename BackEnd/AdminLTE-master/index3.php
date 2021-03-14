938e11de94acd35468664331d31cb22028e4b52c
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-03-13 20:31:06 -0400

    Update index3.php

--- BackEnd/AdminLTE-master/index3.php
+++ BackEnd/AdminLTE-master/index3.php
@@ -72,7 +72,49 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
+
+            <li class="nav-header">Opciones</li>
+              <li class="nav-item">
+                <a href="iframe.html" class="nav-link">
+                  <i class="nav-icon fas fa-ellipsis-h"></i>
+                  <p>Productos</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="https://adminlte.io/docs/3.1/" class="nav-link">
+                  <i class="nav-icon fas fa-file"></i>
+                  <p>Administradores</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="https://adminlte.io/docs/3.1/" class="nav-link">
+                  <i class="nav-icon fas fa-file"></i>
+                  <p>Usuarios</p>
+                </a>
+              </li>
+            <li class="nav-item menu-open">
+          
+            <li class="nav-header">Opciones</li>
+              <li class="nav-item">
+                <a href="iframe.html" class="nav-link">
+                  <i class="nav-icon fas fa-ellipsis-h"></i>
+                  <p>Productos</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="https://adminlte.io/docs/3.1/" class="nav-link">
+                  <i class="nav-icon fas fa-file"></i>
+                  <p>Administradores</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="https://adminlte.io/docs/3.1/" class="nav-link">
+                  <i class="nav-icon fas fa-file"></i>
+                  <p>Usuarios</p>
+                </a>
+              </li>
+            <li class="nav-item menu-open">
+
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
