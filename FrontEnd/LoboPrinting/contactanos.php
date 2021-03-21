cb6de657bd0ae9d7d7228802b7c9e55f067fad83
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-03-21 00:08:35 -0400

    Update contactanos.php

--- FrontEnd/LoboPrinting/contactanos.php
+++ FrontEnd/LoboPrinting/contactanos.php
@@ -139,18 +139,20 @@
         </div>
         <!-- End Offset Wrapper -->
         <!-- Start Bradcaump area -->
-        <div class="ht__bradcaump__area">
-            <div class="ht__bradcaump__wrap">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="bradcaump__inner text-center">
-                                <h2 class="bradcaump-title">Contacto</h2>
-                                <nav class="bradcaump-inner">
-                                  <a class="breadcrumb-item" href="index.html">Inicio</a>
-                                  <span class="brd-separetor">/</span>
-                                  <span class="breadcrumb-item active">Contacto</span>
-                                </nav>
+        <div class="container">
+            <div class="ht__bradcaump__area">
+                <div class="ht__bradcaump__wrap">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="bradcaump__inner text-center">
+                                    <h2 class="bradcaump-title">Contacto</h2>
+                                    <nav class="bradcaump-inner">
+                                    <a class="breadcrumb-item" href="index.html">Inicio</a>
+                                    <span class="brd-separetor">/</span>
+                                    <span class="breadcrumb-item active">Contacto</span>
+                                    </nav>
+                                </div>
                             </div>
                         </div>
                     </div>
