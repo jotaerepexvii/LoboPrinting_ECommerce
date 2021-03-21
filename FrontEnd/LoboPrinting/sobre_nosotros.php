b67c249357605d57a14ad76e72eb03f8d364ee0f
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-03-21 00:48:34 -0400

    Update sobre_nosotros.php

--- FrontEnd/LoboPrinting/sobre_nosotros.php
+++ FrontEnd/LoboPrinting/sobre_nosotros.php
@@ -266,7 +266,7 @@
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore gna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
                         </div>
                         <div class="store__btn">
-                            <a href="contactanos.php">contact us</a>
+                            <a href="contactanos.php">Contáctanos</a>
                         </div>
                     </div>
                 </div>
