49a183935eddadeae4b499ae4e46857546817bab
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-03-21 00:47:37 -0400

    Update contactanos.php

--- FrontEnd/LoboPrinting/contactanos.php
+++ FrontEnd/LoboPrinting/contactanos.php
@@ -290,9 +290,6 @@
                             <h2 class="title__line">Welcome To Tmart Store</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore gna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</p>
                         </div>
-                        <div class="store__btn">
-                            <a href="contactanos.php">contact us</a>
-                        </div>
                     </div>
                 </div>
             </div>
