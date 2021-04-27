d4f65bc321f22dcd135fcbdf344c1d952eb1fcef
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-04-27 00:48:37 -0400

    Last Tweaks of the night

--- BackEnd/login.php
+++ BackEnd/login.php
@@ -1,3 +1,10 @@
+<?php
+  session_start();
+  include 'phpIncludes/connection.php';
+  if (isset($_SESSION['login'])) {
+    header('location:index.php');
+  }
+?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
