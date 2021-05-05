805c9c00519abca8da250a9d4d0edb7d513bb339
Bryan <31052197+Bryantacoronte@users.noreply.github.com>
2021-05-04 23:58:06 -0400

    checkout
    check

--- FrontEnd/checkout.php
+++ FrontEnd/checkout.php
@@ -77,108 +77,174 @@
         <section class="our-checkout-area ptb--120 bg__white">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8 col-lg-8">
-                        <?php
-                            error_reporting(E_ERROR | E_PARSE);
+                    <form action="#" method="post">
+                        <div class="col-md-8 col-lg-8">
+                            <?php
+                                if(isset($_POST['confirmar']))
+                                {
+                                    $errors = array();
 
-                            $query = "SELECT *
-                                        FROM Users
-                                        WHERE user_id = {$_SESSION['login']}";
-                            $r = mysqli_query($dbc,$query);//Make the Query
-                            $row = mysqli_fetch_array($r);
+                                    //Variables
+                                    $user_id = (int)$_POST['user_id'];
+                                    $name = filter_input(INPUT_POST, 'name');
+                                    $lastname = filter_input(INPUT_POST, 'lastname');
+                                    $email = filter_input(INPUT_POST, 'email');
+                                    $phone = filter_input(INPUT_POST, 'phone');
+                                    
+                                    if  (empty($name))
+                                        array_push($errors, 'Name is require!');
+                                    if  (empty($lastname))
+                                        array_push($errors, 'Lastname is require!');
+                                    if  (empty($email))
+                                        array_push($errors, 'Email is require!');
 
-                            print"
-                                <!-- Start Checkbox Area -->
-                                <div class='checkout-form'>
-                                    <h2 class='section-title-3'>Detalles de facturación</h2>
-                                    <div class='checkout-form-inner'>
-                                        <div class='single-checkout-box'>
-                                            <input type='text' placeholder='Nombre*' oninvalid='this.setCustomValidity('Inserte Nombre')' title='Inserte su nombre' value=$row[name] required>
-                                            <input type='text' placeholder='Apellidos*' oninvalid='this.setCustomValidity('Inserte Apellidos')' title='Inserte sus apellidos' value=$row[lastname] required>
-                                        </div>
-                                        <div class='single-checkout-box'>
-                                            <input type='email' placeholder='Correo Electrónico*' oninvalid='this.setCustomValidity('Inserte Correo Electrónico')' title='Inserte su correo electrónico' value=$row[email] required>
-                                            <input type='text' placeholder='Teléfono*' oninvalid='this.setCustomValidity('Inserte Teléfono')' title='Inserte su número de teléfono' value=$row[phone]>
-                                        </div>
+                                    $address_id = (int)$_POST['address_id'];
+                                    $address_1 = filter_input(INPUT_POST, 'address_1');
+                                    $address_2 = filter_input(INPUT_POST, 'address_2');
+                                    $zip_code = filter_input(INPUT_POST, 'zip_code');
+                                    $city = filter_input(INPUT_POST, 'city');
+                                    $state = filter_input(INPUT_POST, 'state');
+                                    
+                                    if  (empty($address_1))
+                                        array_push($errors, 'Address is require!');
+                                    if  (empty($zip_code))
+                                        array_push($errors, 'Zip Code is require!');
+                                    if  (empty($city))
+                                        array_push($errors, 'City is require!');
+                                    if  (empty($state))
+                                        array_push($errors, 'State is require!');
+                                    
+                                    $payment_id = (int)$_POST['payment_id'];
+                                    $card_name = filter_input(INPUT_POST, 'card_name');
+                                    $card_number = filter_input(INPUT_POST, 'card_number');
+                                    $exp_month = filter_input(INPUT_POST, 'exp_month');
+                                    $exp_year = filter_input(INPUT_POST, 'exp_year');
+                                    $ccv = filter_input(INPUT_POST, 'ccv');
+                                    
+                                    if  (empty($card_name))
+                                        array_push($errors, 'Card Name is require!');
+                                    if  (empty($card_number))
+                                        array_push($errors, 'Card Number is require!');
+                                    if  (empty($exp_month))
+                                        array_push($errors, 'Exp Month is require!');
+                                    if  (empty($exp_year))
+                                        array_push($errors, 'Exp Year is require!');
+                                    if  (empty($ccv))
+                                        array_push($errors, 'CCV is require!');
+                                    
+                                    $track_num =  rand(1111111111,getrandmax());
+                                    date_default_timezone_set('Atlantic/Bermuda');
+                                    $my_date = date("Y-m-d H:i:s");
+                                    $status_id = 1;
+
+                                    if(count($errors) == 0)
+                                    {
                                         
-                                    </div>
-                                </div>
-                                <!-- End Checkbox Area -->
-                            ";
+                                        $query_order = "INSERT INTO Order (user_id, order_date, address_id, payment_id, track_number, status_id)
+                                        VALUES ('$user_id', '$my_date', '$address_id', '$payment_id', '$track_num', '$status_id')";
+                                        
+                                        //print "$user_id, $my_date, $address_id, $payment_id, $track_num, $status_id)";
 
-                            $query1 = "SELECT *
-                                        FROM Payment_method
-                                        WHERE user_id = {$_SESSION['login']}";
-                            $r1 = mysqli_query($dbc,$query1);//Make the Query
-                            $row1 = mysqli_fetch_array($r1);
+                                        if(mysqli_query($dbc,$query_order))//Validate Insert
+                                        {
+                                            mysqli_close($dbc);
+                                            //header('Location: administrators.php');
+                                            print 'Todo Bien';
+                                        }
+                                        else
+                                            echo "Error: " . $query_order . "<br>" . mysqli_error($dbc);
+                                    }
+                                     else
+                                            echo '<script>alert("ERROR:Variables")</script>';
+                                }
+                            ?>
+                            
+                            <?php
+                                error_reporting(E_ERROR | E_PARSE);
 
-                            print "
-                                <!-- Start Payment Box -->
-                                <div class='payment-form'>
-                                    <h2 class='section-title-3'>Método de Pago</h2>
-                                    <div class='payment-form-inner'>
-                                        <div class='single-checkout-box'>
-                                            <input type='text' placeholder='Nombre De Tarjeta*' value=$row1[card_name]>
-                                            <input type='text' placeholder='Número De Tarjeta*' value=$row1[card_number]>
-                                        </div>
-                                        <div class='single-checkout-box'>
-                                            <input type='text' placeholder='EXP Month*' value=$row1[exp_month]>
-                                            <input type='text' placeholder='EXP Year*' value=$row1[exp_year]>
-                                        </div>
-                                        <div class='single-checkout-box'>
-                                            <input type='text' placeholder='CCV*' value=$row1[ccv]>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- End Payment Box -->
-                            ";
+                                $query = "SELECT *
+                                            FROM Users
+                                            WHERE user_id = {$_SESSION['login']}";
+                                $r = mysqli_query($dbc,$query);//Make the Query
+                                $row = mysqli_fetch_array($r);
+                            ?>
+                                    <!-- Start Checkbox Area -->
+                                    <div class='checkout-form'>
+                                        <h2 class='section-title-3'>Detalles de facturación</h2>
+                                        <div class='checkout-form-inner'>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='name' placeholder='Nombre' value='<?php echo $row['name'] ?>'>
+                                                <input type='text' name='lastname' placeholder='Número De Tarjeta*' value='<?php echo $row['lastname'] ?>'>
+                                            </div>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='email' placeholder='Email' value='<?php echo $row['email'] ?>'>
+                                                <input type='text' name='phone' placeholder='Telefono' value='<?php echo $row['phone'] ?>'>
+                                            </div>
 
-                            $query2 = "SELECT *
-                                        FROM Address
-                                        WHERE user_id = {$_SESSION['login']}";
-                            $r2 = mysqli_query($dbc,$query2);//Make the Query
-                            $row2 = mysqli_fetch_array($r2);
-
-                            print "
-                                <!-- Start Payment Box -->
-                                <div class='payment-form'>
-                                    <h2 class='section-title-3'>Direccion de Envio</h2>
-                                    <div class='payment-form-inner'>
-                                        <div class='single-checkout-box'>
-                                            <input type='text' placeholder='Direccion 1*' value=$row2[address_1]>
-                                            <input type='text' placeholder='Direccion 2' value=$row2[address_2]>
-                                        </div>
-                                        <div class='single-checkout-box'>
-                                            <input type='text' placeholder='Zip Code*' value=$row2[zip_code]>
-                                            <input type='text' placeholder='Ciudad*' value=$row2[city]>
-                                        </div>
-                                        <div class='single-checkout-box'>
-                                            <input type='text' placeholder='Estado*' value=$row2[state]>
                                         </div>
                                     </div>
-                                    <div class='single-checkout-box'>
-                                        <div class='g-recaptcha' data-sitekey='6LfOd7YaAAAAAKDfXyWBTAbjZKPhhzXg-8jWqExB'></div>
+                                    <!-- End Checkbox Area -->
+                            <?php
+                                $query1 = "SELECT *
+                                            FROM Payment_method
+                                            WHERE user_id = {$_SESSION['login']}";
+                                $r1 = mysqli_query($dbc,$query1);//Make the Query
+                                $row1 = mysqli_fetch_array($r1);
+                            ?>
+                                    <!-- Start Payment Box -->
+                                    <div class='payment-form'>
+                                        <h2 class='section-title-3'>Método de Pago</h2>
+                                        <div class='payment-form-inner'>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='card_name' placeholder='Nombre De Tarjeta*' value='<?php echo $row1['card_name'] ?>'>
+                                                <input type='text' name='card_number' placeholder='Número De Tarjeta*' value='<?php echo $row1['card_number'] ?>'>
+                                            </div>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='exp_month' placeholder='EXP Month*' value='<?php echo $row1['exp_month'] ?>'>
+                                                <input type='text' name='exp_year' placeholder='EXP Year*' value='<?php echo $row1['exp_year'] ?>'>
+                                            </div>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='ccv' placeholder='CCV*' value='<?php echo $row1['ccv'] ?>'>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class='checkout-button'>
-                                    <button class='btnCheckout' href='#'>CONFIRMAR Y COMPRAR</button>
-                                </div>
-                                <!-- End Payment Box -->
-                            ";
-                        ?>
-                    <!--
-                    <div class='our-payment-sestem'>
-                        <h2 class='section-title-3'>Se acepta :</h2>
-                        <ul class='payment-menu'>
-                            <li><a href='#'><img src='images/payment/1.jpg' alt='payment-img'></a></li>
-                            <li><a href='#'><img src='images/payment/2.jpg' alt='payment-img'></a></li>
-                            <li><a href='#'><img src='images/payment/3.jpg' alt='payment-img'></a></li>
-                            <li><a href='#'><img src='images/payment/4.jpg' alt='payment-img'></a></li>
-                            <li><a href='#'><img src='images/payment/5.jpg' alt='payment-img'></a></li>
-                        </ul>
-                    </div>
-                    -->
-                    </div>
+                                    <!-- End Payment Box -->
+                            <?php
+                                $query2 = "SELECT *
+                                            FROM Address
+                                            WHERE user_id = {$_SESSION['login']}";
+                                $r2 = mysqli_query($dbc,$query2);//Make the Query
+                                $row2 = mysqli_fetch_array($r2);
+                            ?>
+                                    <!-- Start Payment Box -->
+                                    <div class='payment-form'>
+                                        <h2 class='section-title-3'>Direccion de Envio</h2>
+                                        <div class='payment-form-inner'>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='address_1' placeholder='Direccion 1*' value='<?php echo $row2['address_1'] ?>'>
+                                                <input type='text' name='address_2' placeholder='Direccion 2' value='<?php echo $row2['address_2'] ?>'>
+                                            </div>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='zip_code' placeholder='Zip Code*' value='<?php echo $row2['zip_code'] ?>'>
+                                                <input type='text' name='city' placeholder='Ciudad*' value='<?php echo $row2['city'] ?>'>
+                                            </div>
+                                            <div class='single-checkout-box'>
+                                                <input type='text' name='state' placeholder='Estado*' value='<?php echo $row2['state'] ?>'>
+                                            </div>
+                                        </div>
+                                        <div class='single-checkout-box'>
+                                            <div class='g-recaptcha' data-sitekey='6LfOd7YaAAAAAKDfXyWBTAbjZKPhhzXg-8jWqExB'></div>
+                                        </div>
+                                    </div>
+                                    <div class='checkout-button'>
+                                        <button class='btnCheckout' name='confirmar'>CONFIRMAR Y COMPRAR</button>
+                                        <input type ='hidden' name='user_id' value='<?php echo $row['user_id'] ?>'>
+                                        <input type ='hidden' name='address_id' value='<?php echo $row2['address_id'] ?>'>
+                                        <input type ='hidden' name='payment_id' value='<?php echo $row1['payment_id'] ?>'>
+                                    </div>
+                                    <!-- End Payment Box -->
+                        </div>
+                    </form>
                     <div class="col-md-4 col-lg-4">
                         <div class="checkout-right-sidebar">
                             <div class="our-important-note">
