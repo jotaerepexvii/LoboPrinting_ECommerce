b5f152e1fd05cb3acdf1eec4abd604f922a9d3b3
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-05-09 15:22:38 -0400

    Update loginRequired.php

--- FrontEnd/loginRequired.php
+++ FrontEnd/loginRequired.php
@@ -60,7 +60,6 @@
                 </div>
             </div>
         </div>
-
         <div class='row align-items-center'>
             <div class='col-md-12 col-lg-12 col-sm-12 col-xs-12 col-md-offset-5'>
                 <div class='htc__choose__wrap'>
