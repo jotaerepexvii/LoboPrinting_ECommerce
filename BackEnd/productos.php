56525a96c0ca009d1b2e53a8d1e76ce35e2df2ee
Jonathan Rodriguez <43053797+jotaerepexvii@users.noreply.github.com>
2021-05-12 13:51:31 -0400

    Update productos.php

--- BackEnd/productos.php
+++ BackEnd/productos.php
@@ -11,13 +11,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   
-    <title>Lobo Printing | Productos</title>
+    <title>Productos | Lobo Printing</title>
     <link rel="icon"  href="dist/img/lobo.ico" type="icon" sizes="16x16">
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+      <!-- DataTables -->
+    <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
     <!-- IonIcons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
@@ -41,7 +45,6 @@
             <?php
                 include './phpIncludes/sidebar.php';
             ?>
-            <!-- /.sidebar -->
         </aside>
 
         <!-- Content Wrapper. Contains page content -->
@@ -51,12 +54,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Productos</h1>
+                            <h1 class="m-0">Productos a la venta</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="index.php">Tablero Administrativo</a></li>
-                                <li class="breadcrumb-item active">Productos</li>
+                                <li class="breadcrumb-item active">Productos a la venta</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
@@ -65,73 +68,77 @@
             <!-- /.content-header -->
 
             <!-- Main content -->
-            <div class="content">
+            <section class="content">
                 <div class="container-fluid">
-                    <div class="card-secondary bg-light border-dark">
-                        <div class="card-header">
-                            <h3 class="card-title">Productos a la venta</h3>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card bg-light border-dark">
+                                <div class="card-header">
+                                    <h3 class="card-title" style="font-weight: bold;">Productos a la venta</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                    <table id="example1" class="table table-bordered table-hover table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Id</th>
+                                                <th title='Click sobre el nombre para ver o editar'>Artículo</th>
+                                                <th>Precio</th>
+                                                <th>Unidades Disponibles</th>
+                                                <th>Unidades Vendidas</th>
+                                                <th>Ver/Editar</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                $query = "SELECT *
+                                                    FROM Product limit 25";
+                                            
+                                                if($r = mysqli_query($dbc, $query))//Save & Validate Query Result
+                                                {
+                                                    while($row=mysqli_fetch_array($r))//Present Products
+                                                    {
+                                                        print "
+                                                            <tr>
+                                                                <td>$row[product_id]</td>
+                                                                <td><a href='detallesProducto.php?product_id={$row['product_id']}'>$row[name] $row[description]<a></td>
+                                                                <td>$$row[price]</td>
+                                                                <td>$row[in_stock]</td>
+                                                                <td>$row[sold]</td>
+                                                                <td>
+                                                                    <a href='editarProducto.php?product_id={$row['product_id']}'>Ver</a>
+                                                                    <a>|</a>
+                                                                    <a href='editarProducto.php?product_id={$row['product_id']}'>Editar</a>
+                                                                </td>
+                                                            </tr>
+                                                        ";
+                                                    }
+                                                }
+                                                else
+                                                    print'<p style="color:red">NO SE PUEDE MOSTRAR RECORD PORQUE:'.mysqli_error($dbc).'.</P>';
+                                                mysqli_close($dbc);
+                                            ?>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Id</th>
+                                                <th>Artículo</th>
+                                                <th>Precio</th>
+                                                <th>Unidades Disponibles</th>
+                                                <th>Unidades Vendidas</th>
+                                                <th>Ver/Editar</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
                         </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <table id="example1" class="table table-bordered table-striped table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>Id</th>
-                                        <th>Artículo</th>
-                                        <th>Precio</th>
-                                        <th>Unidades Disponibles</th>
-                                        <th>Unidades Vendidas</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-                                        $query = "SELECT *
-                                            FROM Product limit 25";
-                                    
-                                        if($r = mysqli_query($dbc, $query))//Save & Validate Query Result
-                                        {
-                                            while($row=mysqli_fetch_array($r))//Present Products
-                                            {
-                                                print "
-                                                    <tr>
-                                                        <td>$row[product_id]</td>
-                                                        <td><a href='detallesProducto.php?product_id={$row['product_id']}'>$row[name] $row[description]<a></td>
-                                                        <td>$$row[price]</td>
-                                                        <td>$row[in_stock]</td>
-                                                        <td>$row[sold]</td>
-                                                        <td>
-                                                            <a href='editarProducto.php?product_id={$row['product_id']}'>Ver</a>
-                                                            <a>|</a>
-                                                            <a href='editarProducto.php?product_id={$row['product_id']}'>Editar</a>
-                                                        </td>
-                                                    </tr>
-                                                ";
-                                            }
-                                        }
-                                        else
-                                            print'<p style="color:red">NO SE PUEDE MOSTRAR RECORD PORQUE:'.mysqli_error($dbc).'.</P>';
-                                        mysqli_close($dbc);
-                                    ?>
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th>Id</th>
-                                        <th>Artículo</th>
-                                        <th>Precio</th>
-                                        <th>Unidades Disponibles</th>
-                                        <th>Unidades Vendidas</th>
-                                        <th></th>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                        </div>
-                        <!-- /.card-body -->
                     </div>
                     <!-- /.row -->
                 </div>
                 <!-- /.container-fluid -->
-            </div>
+            </section>
             <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
@@ -175,13 +182,11 @@
 <script src="plugins/chart.js/Chart.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard3.js"></script>
 <script>
   $(function () {
     $("#example1").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      "buttons": ["excel", "pdf", "print"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     $('#example2').DataTable({
       "paging": true,
